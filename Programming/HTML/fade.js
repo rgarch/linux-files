0a02c60f417c395cfbde676bcf70ca0ac712d1be
rgarch <rs87428@gmail.com>
2012-12-29 18:51:49 -0600

    changed to 'nav'

--- Programming/HTML/fade.js
+++ Programming/HTML/fade.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-    $('div').mouseenter(function() {
-        $('div').fadeTo('fast',1);
+    $('nav').mouseenter(function() {
+        $('nav').fadeTo('fast',1);
     });
-    $('div').mouseleave(function() {
-        $('div').fadeTo('fast', 0.5);
+    $('nav').mouseleave(function() {
+        $('nav').fadeTo('fast', 0.5);
     });
 });
