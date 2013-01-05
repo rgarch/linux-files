af7f09377c2594f8be12751f39efa24a2140aefa
rgarch <rs87428@gmail.com>
2013-01-05 11:14:06 -0600

    click fadeout

--- Programming/HTML/fade.js
+++ Programming/HTML/fade.js
@@ -1,8 +1,5 @@
 $(document).ready(function() {
-    $('#nav ul li a h').mouseenter(function() {
-        $(this).fadeTo('fast',1);
-    });
-    $('#nav ul li a h').mouseleave(function() {
-        $(this).fadeTo('fast', 0.5);
+    $('#nav ul li a h').click(function() {
+        $(this).fadeOut('fast',1);
     });
 });
