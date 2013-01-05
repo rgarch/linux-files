d47614d4ab82aa20ea0cefd3a8cf58ed5e14202e
rgarch <rs87428@gmail.com>
2013-01-05 11:40:40 -0600

    #logo

--- Programming/HTML/fade.js
+++ Programming/HTML/fade.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('#nav ul li a:hover').mouseenter(function() {
-        $('#nav ul li a:hover').fadeOut('slow');
+    $('#logo').mouseenter(function() {
+        $(this).fadeOut('slow');
     });
 });
