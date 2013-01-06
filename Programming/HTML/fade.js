f9686d0104f8f8eaee130efb6bd73aa1bc7db505
rgarch <rs87428@gmail.com>
2013-01-05 22:22:09 -0600

    click ==> fade out

--- Programming/HTML/fade.js
+++ Programming/HTML/fade.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('#logo').mouseenter(function() {
-        $('a').fadeOut('slow');
+    $('#logo').click(function() {
+        $('.a').fadeOut('slow');
     });
 });
