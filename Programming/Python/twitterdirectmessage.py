5ce97265aafdde1f3c03dade0762d6df78a56ce8
rgarch <rs87428@gmail.com>
2013-03-23 05:25:56 -0500

    twitter api direct

--- Programming/Python/twitterdirectmessage.py
+++ Programming/Python/twitterdirectmessage.py
@@ -1,5 +1,12 @@
+#!/usr/bin/python2.4
+import os
 import twitter
+import sys
+import textwrap
+import json
+import time
 
-api = twitter.Api()
+api = twitter.Api(consumer_key='CONSUMER_KEY', consumer_secret='CONSUMER_SECRET', access_token_key='ACCESS_TOKEN_KEY', access_token_secret='ACCESS_TOKEN_SECRET')
 
-messages = api.
+messages = api.PostDirectMessage(raw_input("Twitter ID: "), raw_input("direct message: "))
+print messages.text
