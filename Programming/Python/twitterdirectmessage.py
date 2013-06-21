cb2e31c0ea67680eed6b23eb7e6bc4f43827104c
rgarch <rs87428@gmail.com>
2013-06-21 02:48:48 -0500

    updated direct message output to new version

--- Programming/Python/twitterdirectmessage.py
+++ Programming/Python/twitterdirectmessage.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python2.4
+#!/usr/bin/python2
 import os
 import twitter
 import sys
@@ -8,5 +8,13 @@ import time
 
 api = twitter.Api(consumer_key='CONSUMER_KEY', consumer_secret='CONSUMER_SECRET', access_token_key='ACCESS_TOKEN_KEY', access_token_secret='ACCESS_TOKEN_SECRET')
 
+directM = api.GetDirectMessages()
+dparse = [s.text for s in directM]
+dout = dparse[0]
+print dout
+
 messages = api.PostDirectMessage(raw_input("Twitter ID: "), raw_input("direct message: "))
 print messages.text
+
+#status = api.PostUpdate(raw_input("status: "))
+#print status.text
