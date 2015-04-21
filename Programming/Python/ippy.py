20c490d7487859144d35e9557b616039d56f098d
rgarch <rs87428@gmail.com>
2015-04-21 16:03:10 -0500

    add a newline

--- Programming/Python/ippy.py
+++ Programming/Python/ippy.py
@@ -18,7 +18,7 @@ smtpserver.ehlo()
 smtpserver.starttls()
 smtpserver.ehlo
 smtpserver.login(gmail_user, gmail_pwd)
-header = 'To:' + to + '\n' + 'From: ' + gmail_user + '\n' + 'Subject:System IP\n'
+header = 'To:' + to + '\n' + 'From: ' + gmail_user + '\n' + 'Subject:System IP\n\n'
 msg = header+str(ip) 
 smtpserver.sendmail(gmail_user, to, msg)
 smtpserver.close()
